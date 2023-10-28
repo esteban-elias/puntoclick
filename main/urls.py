352bb6a61d61d3316f83098b85ee474940b39482
esteban-elias <esteban.elias@proton.me>
2023-10-28 14:09:43 -0300

    Crear página para mostrar categoría

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('categorias/<int:id_categoria>', views.categoria, name='categoria'),
 ]
--- main/views.py
+++ main/views.py
@@ -8,3 +8,13 @@ def index(request):
         'categorias': categorias
     }
     return render(request, 'index.html', context)
+
+
+def categoria(request, id_categoria):
+    categoria = Categoria.objects.get(pk=id_categoria)
+    productos = categoria.producto_set.all()
+    context = {
+        'categoria': categoria,
+        'productos': productos
+    }
+    return render(request, 'categoria.html', context)
